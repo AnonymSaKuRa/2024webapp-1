3a8bcbca68c075ce308ca9bbe7240037420361fc
RikushiSakurai <rikopinn0928@gmail.com>
2024-10-01 17:21:42 +0900

    HTMLの加筆と追加など

---
+++ js/exhi-regist.js
@@ -0,0 +1,37 @@
+// 情報登録画面用
+const genderBtn = document.querySelectorAll(".btn");
+const slideshow = document.querySelector(".slideshow");
+const next = document.querySelectorAll(".next");
+const meter = document.querySelector(".meter");
+
+let translate = 0;
+let num = 12.5;
+let newNum = 0;
+// 男性、女性のボタンの処理
+for (let i = 0; i < genderBtn.length; i++) {
+    genderBtn[i].addEventListener("click", () => {
+        translate += num;
+        slideshow.style.transform = `translateX(-${translate}%)`;
+        newNum += num;
+        meter.style.width = `${newNum}%`;
+    });
+}
+
+// 次へを押したときのボタンの処理
+for (let i = 0; i < next.length; i++) {
+    next[i].addEventListener("click", () => {
+        translate += num;
+        slideshow.style.transform = `translateX(-${translate}%)`;
+        newNum += num;
+        meter.style.width = `${newNum}%`;
+    });
+}
+
+// 次へを押したときのボタンの処理
+const prev = document.querySelector(".prev");
+prev.addEventListener("click", () => {
+    translate -= num;
+    slideshow.style.transform = `translateX(-${translate}%)`;
+    newNum += num;
+    meter.style.width = `${newNum}%`;
+});
---
+++ js/index.js
@@ -0,0 +1,17 @@
+/*
+    ロゴのアニメーションが終わったらメインのコンテンツが出てくる
+    indexページを作っている間はコメントアウトで処理されないようにしておく
+    cssの方も.contentsのtransformがコメントアウトされている
+*/
+// const content = document.querySelector(".contents");
+// function dispOpen() {
+//     content.classList.add("active");
+// }
+
+// setTimeout(dispOpen, 6000);
+
+const googleBtn = document.querySelector(".google_flex");
+googleBtn.addEventListener("click", () => {
+    document.querySelector(".contents").classList.add("click_google_btn");
+    document.querySelector(".select").classList.add("click_google_btn_active");
+});
