64e4ced8377e85e6410ae0e0f841ecaf009b6634
RikushiSakurai <rikopinn0928@gmail.com>
2024-10-02 14:26:55 +0900

    進んだ分

--- js/exhi-regist.js
+++ js/exhi-regist.js
@@ -3,35 +3,32 @@ const genderBtn = document.querySelectorAll(".btn");
 const slideshow = document.querySelector(".slideshow");
 const next = document.querySelectorAll(".next");
 const meter = document.querySelector(".meter");
+const prev = document.querySelector(".prev");
 
-let translate = 0;
-let num = 12.5;
-let newNum = 0;
-// 男性、女性のボタンの処理
-for (let i = 0; i < genderBtn.length; i++) {
-    genderBtn[i].addEventListener("click", () => {
-        translate += num;
-        slideshow.style.transform = `translateX(-${translate}%)`;
-        newNum += num;
-        meter.style.width = `${newNum}%`;
-    });
+let translate = 0;      // translateXの初期値
+const num = 12.5;       // コンテンツを動かす値
+const widthNum = 16.6;  // メーターを動かすための値
+let newNum = 0;         // メーターの初期値
+
+// translateXを動かすための関数
+function slideMove(moveNum, moveWidth) {
+    translate += moveNum;
+    slideshow.style.transform = `translateX(-${translate}%)`;
+    newNum += moveWidth;
+    meter.style.width = `${newNum}%`;
 }
 
 // 次へを押したときのボタンの処理
 for (let i = 0; i < next.length; i++) {
     next[i].addEventListener("click", () => {
-        translate += num;
-        slideshow.style.transform = `translateX(-${translate}%)`;
-        newNum += num;
-        meter.style.width = `${newNum}%`;
+        slideMove(num, widthNum);
     });
 }
 
-// 次へを押したときのボタンの処理
-const prev = document.querySelector(".prev");
+// 戻るを押したときのボタンの処理
 prev.addEventListener("click", () => {
     translate -= num;
     slideshow.style.transform = `translateX(-${translate}%)`;
-    newNum += num;
+    newNum -= widthNum;
     meter.style.width = `${newNum}%`;
 });
